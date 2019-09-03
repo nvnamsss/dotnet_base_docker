fba993674378072e9fb2efa2c3994f31743528f2
xnguyen9a100 <nguyen.nguyen@eyeq.tech>
2019-09-03 14:43:15 +0700

    fix an issue that caused image failed to be pushed into dockerhub

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 docker login -u nvnam -p "nvn22031998"
 docker build --no-cache . -t dotnet-base-docker:latest
 docker tag dotnet-base-docker:latest nvnam/dotnet-base-docker:latest
-docker push dotnet-base-docker:latest
+docker push nvnam/dotnet-base-docker:latest
